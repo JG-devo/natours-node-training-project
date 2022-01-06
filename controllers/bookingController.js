45df24ba422c453167fd97f69419be49b687d5c2
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2022-01-06 16:15:27 +0000

    bug fix: booking not being added to database

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -29,7 +29,11 @@ exports.getCheckoutSession = catchAsync(async (req, res) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
         },
       },
@@ -75,7 +79,7 @@ exports.webhookCheckout = (req, res) => {
     return res.status(400).send(`Webhook error: ${err.message}`); // Error goes back to stripe themselves with send method
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
